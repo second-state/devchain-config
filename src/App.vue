17d3c81bf4e9adbda760d6da54ca372a35120577
shenzhun <shenzhunallen@gmail.com>
2019-05-10 21:19:01 -0700

    init genesis generator

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GenesisGenerator msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GenesisGenerator from './components/GenesisGenerator.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GenesisGenerator
   }
 }
 </script>
---
+++ src/components/GenesisGenerator.vue
@@ -0,0 +1,31 @@
+<template>
+<json-editor ref="JsonEditor" :schema="schema" v-model="model">
+    <button @click="submit">submit</button>
+    <button @click="reset">Reset</button>
+</json-editor>
+</template>
+
+<script>
+// import vue-json-ui-editor
+import JsonEditor from 'vue-json-ui-editor';
+export default {
+  components: { JsonEditor },
+  data: () => ({
+    schema: require('@/schema/genesis'),
+    // data
+    model: {
+      genesis_time: '0001-01-01T00:00:00Z',
+      chain_id: 'local'
+    },
+  }),
+
+  methods: {
+    submit(_e) {
+      alert(JSON.stringify(this.model));
+    },
+    reset() {
+      this.$refs.JsonEditor.reset();
+    },
+  },
+};
+</script>
