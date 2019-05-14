464b3860436adac811bac7461b20f4301127f4f2
shenzhun <shenzhunallen@gmail.com>
2019-05-14 00:48:14 -0700

    update http port to 80

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     devServer: {
         host: '0.0.0.0',
-        port: '8080',
-        public: '34.74.21.253:8080',
+        port: '80',
+        public: '34.74.21.253:80',
     },
 }
